a1c2aa2aeb0d43c59d58b8b7214f607f6027ebf3
62savage <6s2avage@gmail.com>
2024-03-20 23:23:56 +0900

    feat : Box가 컴퓨터라면 결과를 반대로 출력하기

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-export default function Box({ title, item }) {
+export default function Box({ title, item, result }) {
+  let boxResult;
+
+  if (title === "Computer" && result !== "tie" && result !== "") {
+    boxResult = result === "win" ? "lose" : "win";
+  } else {
+    boxResult = result;
+  }
+
   return (
-    <div className="box">
+    <div className={`box ${result && boxResult}`}>
       <h1>{title}</h1>
-      <img src={item && item.img} alt="rock" className="item-img" />
-      <h2>WIN</h2>
+      <img
+        src={item && item.img}
+        alt={item && item.desc}
+        className="item-img"
+      />
+      <h2>{result && boxResult}</h2>
     </div>
   );
 }
